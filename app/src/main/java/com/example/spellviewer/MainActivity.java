8122e605fb5a9f74badac41fa6975877fb86bfa5
felix_reichenbach <felix.reichenbach@uni-tuebingen.de>
2023-10-25 18:39:46 +0200

    Button works, spell list moved to second activity

--- app/src/main/java/com/example/spellviewer/MainActivity.java
+++ app/src/main/java/com/example/spellviewer/MainActivity.java
@@ -7,19 +7,9 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static Resources resources;
-//    private Button goToSpellsButton = (Button) findViewById(R.id.goToSpellsButton);
-    List<Spell> spells;
-
-    ExpandableListView expandableListView;
-    ExpandableListAdapter expandableListAdapter;
 
 
     @Override
@@ -27,19 +17,10 @@ public class MainActivity extends AppCompatActivity {
         resources = getResources();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        goToSpellsButton.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                startActivity(new Intent(MainActivity.this, MainActivity2.class));
-//            }
-//        });
-        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
-        spells = ExpandableListData.getData();
-        expandableListAdapter = new com.example.spellviewer.ExpandableListAdapter(this, spells);
-        expandableListView.setAdapter(expandableListAdapter);
 
     }
 
-//    public void goToSpellList(View view) {
-//        startActivity(new Intent(this, MainActivity2.class));
-//    }
+    public void goToSpellList(View view) {
+        startActivity(new Intent(this, MainActivity2.class));
+    }
 }
