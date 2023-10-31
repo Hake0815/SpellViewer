fa4880d850a715f14494063704aa7030932bd01a
felix_reichenbach <felix.reichenbach@uni-tuebingen.de>
2023-10-31 17:32:29 +0100

    Added more avatars and possibility to add image from gallery.

--- app/src/main/java/com/example/spellviewer/ImageSelectionActivity.java
+++ app/src/main/java/com/example/spellviewer/ImageSelectionActivity.java
@@ -6,14 +6,20 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.FileOutputStream;
@@ -26,11 +32,13 @@ import java.util.List;
 public class ImageSelectionActivity extends AppCompatActivity {
     private List<CheckImageView> imageViews;
     private CheckImageView selectedImageView;
+    private SerialBitmap imageFromGallery;
+    private ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_selection);
-        TableLayout tableLayout = (TableLayout) findViewById(R.id.simpleTableLayout);
+        TableLayout tableLayout = findViewById(R.id.simpleTableLayout);
         imageViews = new ArrayList<>();
         selectedImageView = null;
         View.OnClickListener onClickListener = v -> imageViewSelected((CheckImageView) v);
@@ -47,6 +55,29 @@ public class ImageSelectionActivity extends AppCompatActivity {
                 }
             }
         }
+        pickMedia =
+                registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                    // Callback is invoked after the user selects a media item or closes the
+                    // photo picker.
+                    if (uri != null) {
+                        Bitmap bitmap;
+                        try {
+                            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+//                        Matrix matrix = new Matrix();
+//                        matrix.postRotate(90);
+//                        Bitmap bmp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                        DisplayMetrics displayMetrics = new DisplayMetrics();
+                        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                        int displayWidth = displayMetrics.widthPixels;
+                        int imageWidth = bitmap.getWidth();
+                        int imageHeight = bitmap.getHeight();
+                        imageFromGallery = new SerialBitmap(getResizedBitmap(bitmap,displayWidth*imageWidth/imageHeight,displayWidth));
+                    }
+                });
+
     }
 
     private void imageViewSelected(CheckImageView imageView){
@@ -62,17 +93,23 @@ public class ImageSelectionActivity extends AppCompatActivity {
         selectedImageView = imageView;
     }
     public void confirmImage(View view) {
-        EditText editText = (EditText) findViewById(R.id.editTextText);
+        EditText editText = findViewById(R.id.editTextText);
         String name = String.valueOf(editText.getText());
-        Bitmap image;
-        Intent intent = new Intent();
-        if (selectedImageView == null) {
-            image = BitmapFactory.decodeResource(MainActivity.resources, R.drawable.mysterion_nobg);
+        CharacterImage characterImage;
+        if (imageFromGallery != null) {
+            characterImage = new CharacterImage(name, imageFromGallery);
         } else {
-            image = drawableToBitmap(selectedImageView.getDrawable());
+            Bitmap image;
+            if (selectedImageView == null) {
+                image = BitmapFactory.decodeResource(MainActivity.resources, R.drawable.mysterion_nobg);
+            } else {
+                image = drawableToBitmap(selectedImageView.getDrawable());
+            }
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            SerialBitmap serialImage = new SerialBitmap(getResizedBitmap(image,displayMetrics.widthPixels,displayMetrics.widthPixels));
+            characterImage = new CharacterImage(name,serialImage);
         }
-        SerialBitmap serialImage = new SerialBitmap(image);
-        CharacterImage characterImage = new CharacterImage(name,serialImage);
         try {
             FileOutputStream fos = getApplicationContext().openFileOutput("tempFile", Context.MODE_PRIVATE);
             ObjectOutputStream os = new ObjectOutputStream(fos);
@@ -82,9 +119,16 @@ public class ImageSelectionActivity extends AppCompatActivity {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        Intent intent = new Intent();
         intent.putExtra("result", "tempFile");
         setResult(Activity.RESULT_OK,intent);
         finish();
+    }
+    public void openGallery(View view) {
+        pickMedia.launch(new PickVisualMediaRequest.Builder()
+                .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                .build());
+
     }
 
     public static Bitmap drawableToBitmap (Drawable drawable) {
@@ -108,5 +152,23 @@ public class ImageSelectionActivity extends AppCompatActivity {
         drawable.draw(canvas);
         return bitmap;
     }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+
 
 }
